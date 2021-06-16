b5a9ab920e053d86ae4a490cd6da9876e5411825
Özgün Deniz <ozgundenz@gmail.com>
2021-06-16 22:32:28 +0300

    Last Commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Sunucudan dönen verilerin diğer tüm fonksiyonlarca erişilebilmesi için global değişken tanımladık.
-let sunucudanGelen;
+let sunucudanDonen;
 
 //Sunucuya bağlanmak için bir baglanti nesnesi türettik.
 const baglanti = new XMLHttpRequest();
@@ -7,9 +7,75 @@ const baglanti = new XMLHttpRequest();
 //baglanti nesnesi sunucu bağlantısına hazır olduğunda json dosyasından verileri çekilerek döndürülmesini sağladık.
 baglanti.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-    
-        sunucudanGelen=JSON.parse(baglanti.responseText);
+        sunucudanDonen=JSON.parse(baglanti.responseText);
         soruGetir();
     }
-    return sunucudanGelen;
+    return sunucudanDonen;
 };
+baglanti.open("GET" , "data.json" , true);
+baglanti.send();
+
+const sonucAlani = document.getElementsByClassName("soruAlani")[0];
+const soru = document.getElementById("soru");
+const secenekler = document.getElementsByName(".secenek");
+const aciklamaA = document.getElementById("aAciklama");
+const aciklamaB = document.getElementById("bAciklama");
+const aciklamaC = document.getElementById("cAciklama");
+const aciklamaD = document.getElementById("dAciklama");
+
+const gonderButonu=document.getElementById("gonder");
+
+let puan = 0 ;
+let sira = 0 ;
+
+function soruGetir(){
+    secimiTemizle();
+    console.log(sunucudanDonen);
+
+    let siradakiSoru = sunucudanDonen.sorular[sira];
+
+    soru.innerHTML= siradakiSoru.soru;
+    aciklamaA.innerText=siradakiSoru.secenekA;
+    aciklamaB.innerText=siradakiSoru.secenekB;
+    aciklamaC.innerText=siradakiSoru.secenekC;
+    aciklamaD.innerText=siradakiSoru.secenekD;
+
+}
+
+function secimiTemizle(){
+    secenekler.forEach(secenek=> secenek.checked=false );
+
+}
+
+function secimiAl(){
+    let secim;
+    secenekler.forEach(secenekler =>{
+        if(secenek.checked ==true){
+            secim = secenek.id;
+        }
+    })
+    return secim;
+
+}
+
+gonderButonu.addEventListener("click" , ()=> {
+    const secilen = secimiAl();
+
+    if(secilen){
+        if(secilen === sunucudanDonen.sorular[sira].dogruCevap){
+            puan++;
+        }
+    }
+    sira++;
+
+    if(sira < sunucudanDonen.sorular.length){
+        soruGetir();
+    }
+    else {
+        sonucAlani.innerHTML= 
+        `      <h2>Mevcut sorular içerisinden ${puan}/${sunucudanDonen.sorular.length} oranında başarı sağladınız.</h2>`
+        gonderButonu.setAttribute("onclick","location.reload()");
+        gonderButonu.innerHTML=("Yeniden Başla");
+    }
+})
+
